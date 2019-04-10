b1635313708a5823ab33819365dc7cce0a8d7a09
Yulia Chashchina <chashina_yulia@mail.ru>
2019-04-10 10:11:05 +0300

    import Google Maps and Google Places, get current user's location, add button to return to current position

--- GoogleMapsTask/ViewController.swift
+++ GoogleMapsTask/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
-class ViewController: UIViewController {
 
+///отображает главный экран с гугл картой
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    
+    // MARK: - Properties
+    
+    var mapView: GMSMapView?
+    var locationManager = CLLocationManager()
+
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initiateMap()
+        
+    }
+    
+    
+    // MARK: Functions
+    
+    func initiateMap() {
+
+        let camera = GMSCameraPosition()
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        self.view = mapView
+        mapView?.isMyLocationEnabled = true
+        mapView?.settings.myLocationButton = true
+        
+        //Location Manager code to fetch current location
+        self.locationManager.delegate = self
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    
+    // MARK: - Location Manager delegates
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!, zoom: 17.0)
+        
+        self.mapView?.animate(to: camera)
+        
+        //Finally stop updating location otherwise it will come again and again in this delegate
+        self.locationManager.stopUpdatingLocation()
+        
     }
 
 
